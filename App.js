f68ec2c433cd67a22d2fdd9ff750ab7337b706a5
adampagels <pagels@hotmail.ca>
2020-05-10 22:59:43 -0400

    Import config and initialize firebase

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { firebaseConfig } from "./config";
+import * as firebase from "firebase";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   return (
@@ -12,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
