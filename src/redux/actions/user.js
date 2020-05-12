5b7f03e19ec8728a2fdef8f40eb579a83f7a289c
adampagels <pagels@hotmail.ca>
2020-05-12 18:05:40 -0400

    Create action for setting user Email in Redux store

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,8 +1,16 @@
 export const SET_USERNAME = "SET_USERNAME";
+export const SET_EMAIL = "SET_EMAIL";
 
 export const setUsername = (user) => {
   return {
     type: "SET_USERNAME",
     payload: user,
+  };
+};
+
+export const setEmail = (email) => {
+  return {
+    type: "SET_EMAIL",
+    payload: email,
   };
 };
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,13 +1,16 @@
-import { SET_USERNAME } from "../actions/user";
+import { SET_USERNAME, SET_EMAIL } from "../actions/user";
 
 const initialState = {
   user: null,
+  email: null,
 };
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_USERNAME:
       return { ...state, user: action.payload };
+    case SET_EMAIL:
+      return { ...state, email: action.payload };
     default:
       return state;
   }
