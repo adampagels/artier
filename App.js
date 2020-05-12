f20e2c07e5b196582626e0af7b2e8096b9fb7585
adampagels <pagels@hotmail.ca>
2020-05-11 23:33:15 -0400

    Create store and wrap Navigation with Provider

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { firebaseConfig } from "./config";
 import * as firebase from "firebase";
+import { Provider } from "react-redux";
 import Navigation from "./src/navigation/Navigation";
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import userReducer from "./src/redux/reducers/user";
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
 export default function App() {
-  return <Navigation />;
+  const store = createStore(userReducer, applyMiddleware(thunkMiddleware));
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
 }
