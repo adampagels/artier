b7e76dd90abfb7cf941abc75a6ca4f8c5f2e4bdd
adampagels <pagels@hotmail.ca>
2020-05-11 20:52:36 -0400

    Import Navigation

--- App.js
+++ App.js
@@ -1,25 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { firebaseConfig } from "./config";
 import * as firebase from "firebase";
+import Navigation from "./src/navigation/Navigation";
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
