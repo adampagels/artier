670dc52bcdbcf42f69762e6b69b2aedd0930a9bc
adampagels <pagels@hotmail.ca>
2020-05-12 22:10:42 -0400

    Add buttons for completing registration and allowing location

--- src/screens/LocationScreen.js
+++ src/screens/LocationScreen.js
@@ -1,18 +1,42 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function LocationScreen() {
   return (
     <View style={styles.container}>
-      <Text>LocationScreen</Text>
+      <View>
+        <Text>Enable Location</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Allow Location</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Register</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
     flex: 1,
+  },
+  buttonContainer: {
+    marginTop: 330,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#494E58",
+    borderRadius: 50,
     justifyContent: "center",
+    height: 50,
+    marginBottom: 30,
+    marginHorizontal: 80,
+  },
+  buttonText: {
+    color: "#fefefe",
+    fontWeight: "500",
   },
 });
