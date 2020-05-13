5aed0e4a4d6e38e6f6ba6a7c80c069d1adaac5d7
adampagels <pagels@hotmail.ca>
2020-05-12 22:47:40 -0400

    Ask user permission to use their location

--- src/screens/LocationScreen.js
+++ src/screens/LocationScreen.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
 
 export default function LocationScreen() {
+  const [location, setLocation] = useState("");
+
+  const getLocation = async () => {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status === "granted") {
+      let result = await Location.getCurrentPositionAsync({});
+
+      if (result) {
+        setLocation(result);
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View>
         <Text>Enable Location</Text>
       </View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={!location ? styles.button : styles.locationNotAllowedButton}
+          onPress={getLocation}
+        >
           <Text style={styles.buttonText}>Allow Location</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={location ? styles.button : styles.locationNotAllowedButton}
+        >
           <Text style={styles.buttonText}>Register</Text>
         </TouchableOpacity>
       </View>
@@ -29,6 +49,15 @@ const styles = StyleSheet.create({
   button: {
     alignItems: "center",
     backgroundColor: "#494E58",
+    borderRadius: 50,
+    justifyContent: "center",
+    height: 50,
+    marginBottom: 30,
+    marginHorizontal: 80,
+  },
+  locationNotAllowedButton: {
+    alignItems: "center",
+    backgroundColor: "#8A8F9E",
     borderRadius: 50,
     justifyContent: "center",
     height: 50,
