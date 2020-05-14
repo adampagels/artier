c01116ac1f14dc36056110f596632eee41dc7a19
adampagels <pagels@hotmail.ca>
2020-05-14 13:36:13 -0400

    Add fix for missing Crypto variable error

--- App.js
+++ App.js
@@ -7,6 +7,21 @@ import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import userReducer from "./src/redux/reducers/user";
 
+//Fix "can't find variable: Crypto" error
+import { decode, encode } from "base-64";
+global.crypto = require("@firebase/firestore");
+global.crypto.getRandomValues = (byteArray) => {
+  for (let i = 0; i < byteArray.length; i++) {
+    byteArray[i] = Math.floor(256 * Math.random());
+  }
+};
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
+}
+
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
