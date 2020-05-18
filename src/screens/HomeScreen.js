c2286eb96f6bff89a22784896a650ab966fddd63
adampagels <pagels@hotmail.ca>
2020-05-18 15:47:34 -0400

    Return Swiper with data from art state in Redux store

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,40 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet, Image, ImageBackground } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllArt } from "./../redux/actions/art";
+import * as firebase from "firebase/app";
+import Swiper from "react-native-deck-swiper";
+import Card from "../components/Card";
 
 export default function HomeScreen() {
+  const dispatch = useDispatch();
+  const art = useSelector((state) => state.artReducer.allArt);
+
+  const logOutUser = () => {
+    firebase.auth().signOut();
+  };
+
+  useEffect(() => {
+    dispatch(fetchAllArt());
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>HomeScreen</Text>
+      <Swiper
+        cards={art}
+        renderCard={(card) => <Card card={card} />}
+        onSwiped={(cardIndex) => {
+          console.log(cardIndex);
+        }}
+        onSwipedAll={() => {
+          console.log("onSwipedAll");
+        }}
+        cardIndex={0}
+        backgroundColor={"red"}
+        stackSize={3}
+        verticalSwipe={false}
+      ></Swiper>
+      <Text onPress={() => logOutUser()}>Logout</Text>
     </View>
   );
 }
@@ -14,5 +44,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flex: 1,
     justifyContent: "center",
+  },
+  card: {
+    flex: 1,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
+    justifyContent: "center",
+    backgroundColor: "white",
+  },
+  background: {
+    height: "100%",
+    resizeMode: "stretch",
   },
 });
