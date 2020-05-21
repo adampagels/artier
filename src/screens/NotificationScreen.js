92707c3dfd44a4899d8a7958b187411bee14891b
adampagels <pagels@hotmail.ca>
2020-05-21 17:29:54 -0400

    Show only the current users art with most recent likers in FlatList

--- src/screens/NotificationScreen.js
+++ src/screens/NotificationScreen.js
@@ -1,11 +1,52 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  SafeAreaView,
+  Image,
+} from "react-native";
+import { useSelector } from "react-redux";
 
 export default function NotificationScreen() {
+  const art = useSelector((state) => state.artReducer.userArt);
+
   return (
-    <View style={styles.container}>
-      <Text>NotificationScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={art}
+        scrollEnabled={false}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              flex: 1,
+              alignContent: "right",
+              width: 350,
+            }}
+          >
+            {Object.values(item.data.likes).length > 0 && (
+              <View
+                style={{
+                  flexDirection: "row-reverse",
+                  justifyContent: "flex-end",
+                  marginTop: 30,
+                }}
+              >
+                <Text>
+                  {Object.values(item.data.likes) + " liked your art"}
+                </Text>
+                <Image
+                  source={{ uri: item.data.image.uri }}
+                  style={{ height: 80, width: 80, marginLeft: 10 }}
+                  imageStyle={{ borderRadius: 100 }}
+                ></Image>
+              </View>
+            )}
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
