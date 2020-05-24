49a39dc50eafe5178738d48e0969d8525242116c
adampagels <pagels@hotmail.ca>
2020-05-24 13:09:22 -0400

    Style cards with number of likes and dislikes with Ionicons

--- src/screens/MasterpiecesScreen.js
+++ src/screens/MasterpiecesScreen.js
@@ -8,6 +8,7 @@ import {
   Image,
 } from "react-native";
 import { useSelector } from "react-redux";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function MasterpiecesScreen() {
   const art = useSelector((state) => state.artReducer.allArt);
@@ -20,44 +21,72 @@ export default function MasterpiecesScreen() {
   });
 
   return (
-    <View style={styles.container}>
-      <SafeAreaView style={styles.container}>
-        <FlatList
-          data={highestRatedArt}
-          renderItem={({ item }) => (
-            <View
-              style={{
-                flex: 1,
-                alignContent: "right",
-                width: 350,
-              }}
-            >
-              {Object.values(item.data.likes).length > 0 && (
-                <View
-                  style={{
-                    flexDirection: "row-reverse",
-                    justifyContent: "flex-end",
-                    marginTop: 30,
-                  }}
-                >
-                  <Text>
-                    {Object.values(item.data.likes).length + " likes"}
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={highestRatedArt.splice(0, 10)}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              flex: 1,
+              width: 350,
+              flexDirection: "row",
+              justifyContent: "center",
+            }}
+          >
+            {Object.values(item.data.likes).length > 0 && (
+              <View
+                style={{
+                  marginTop: 60,
+                  shadowColor: "#333",
+                  shadowOffset: {
+                    width: 0,
+                    height: 2,
+                  },
+                  shadowRadius: 6,
+                  shadowOpacity: 0.3,
+                  elevation: 2,
+                }}
+              >
+                <Image
+                  source={{ uri: item.data.image.uri }}
+                  style={{ height: 400, width: 300 }}
+                ></Image>
+                <View style={styles.interactionContainer}>
+                  <Ionicons name="ios-thumbs-up" size={40} color={"#088001"} />
+                  <Text
+                    style={{
+                      color: "#088001",
+                      fontSize: 30,
+                      marginTop: 5,
+                      marginLeft: 10,
+                    }}
+                  >
+                    {Object.values(item.data.likes).length}
                   </Text>
-                  <Text>
-                    {Object.values(item.data.dislikes).length + " dislikes "}
+                  <Ionicons
+                    name="ios-thumbs-down"
+                    size={40}
+                    color={"#FE0F00"}
+                    style={{ marginLeft: 40, marginTop: 7 }}
+                  />
+                  <Text
+                    style={{
+                      color: "#FE0F00",
+                      fontSize: 30,
+                      marginTop: 5,
+                      marginLeft: 10,
+                    }}
+                  >
+                    {Object.values(item.data.dislikes).length}
                   </Text>
-                  <Image
-                    source={{ uri: item.data.image.uri }}
-                    style={{ height: 80, width: 80, marginLeft: 10 }}
-                    imageStyle={{ borderRadius: 100 }}
-                  ></Image>
                 </View>
-              )}
-            </View>
-          )}
-        />
-      </SafeAreaView>
-    </View>
+              </View>
+            )}
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -65,6 +94,10 @@ const styles = StyleSheet.create({
   container: {
     alignItems: "center",
     flex: 1,
+  },
+  interactionContainer: {
+    flexDirection: "row",
     justifyContent: "center",
+    marginTop: 3,
   },
 });
