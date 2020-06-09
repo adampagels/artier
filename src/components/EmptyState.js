ca131e3711d6c3caef562ce62b30088e25f4de20
adampagels <pagels@hotmail.ca>
2020-06-08 22:36:24 -0400

    Return image and font only when both have successfully loaded

--- src/components/EmptyState.js
+++ src/components/EmptyState.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, Image, Text } from "react-native";
 import { useFonts, FingerPaint_400Regular } from "@expo-google-fonts/dev";
 
 export default function ProfileHeaderButtons(props) {
+  const [isReady, setReady] = useState(false);
   const [fontsLoaded] = useFonts({
     FingerPaint_400Regular,
   });
@@ -11,8 +12,9 @@ export default function ProfileHeaderButtons(props) {
       <Image
         source={require("../assets/emptyStateBG.jpg")}
         style={styles.background}
+        onLoadEnd={() => setReady(true)}
       />
-      {fontsLoaded && (
+      {fontsLoaded && isReady && (
         <Text style={styles.text}>
           <Text style={{ color: "#133750" }}>
             {props.lineOne}
@@ -34,6 +36,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "row",
+    alignContent: "center",
+    justifyContent: "center",
   },
   background: {
     borderRadius: 300,
