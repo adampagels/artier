7a9ebd7fcfa5558609216fa8d24d053128e2c011
adampagels <pagels@hotmail.ca>
2020-06-09 23:12:37 -0400

    Navigate user to App navigation if they are signed in

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import * as firebase from "firebase";
 
-export default function LoadingScreen() {
+export default function LoadingScreen(props) {
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      props.navigation.navigate(user ? "App" : "Auth");
+    });
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Loading...</Text>
